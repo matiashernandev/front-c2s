f899cf988184d8f85c2eca2c2eb07db3a7fb709a
nkwaaaa <nkwaaaa@gmail.com>
2023-08-11 12:47:03 -0300

    add tests home

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,7 @@
-import { expect, test, vi } from "vitest"
-import { render, screen, within } from "@testing-library/react"
-import { useRouter } from "next/router"
+import Home, { getStaticProps } from "@/pages"
+import { render, screen } from "@testing-library/react"
+import React from "react"
+//import { getCharacters } from "@/service"
 
 vi.mock("next/router", () => ({
   useRouter: () => ({
@@ -9,6 +10,82 @@ vi.mock("next/router", () => ({
   }),
 }))
 
-vi.mock("../src/services/index.ts", () => ({
+vi.mock("../src/services", () => ({
   getCharacters: vi.fn(),
 }))
+
+/* function MockImage(props: any) {
+  const { priority, ...otherProps } = props
+  return React.createElement("img", {
+    ...otherProps,
+    priority: priority ? "true" : undefined,
+  })
+}
+vi.mock("next/image", () => MockImage) */
+
+describe("Home", () => {
+  it("Should render Heading", () => {
+    render(<Home characters={[]} />)
+
+    const heading = screen.getByText("Amiibos")
+
+    expect(heading).toBeDefined()
+
+    //screen.debug()
+  })
+
+  it("Should render list characters", async () => {
+    const characters2 = [
+      {
+        tail: "1",
+        name: "Mario",
+        image:
+          "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00000000-00340102.png",
+        amiiboSeries: "Super Smash Bros.",
+        character: "Mario",
+        gameSeries: "Super Mario",
+        head: "00000000",
+        release: {
+          au: "2014-11-29",
+          eu: "2014-11-28",
+          jp: "2014-12-06",
+          na: "2014-11-21",
+        },
+        type: "Figure",
+      },
+      {
+        tail: "2",
+        name: "Luigi",
+        image:
+          "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00010000-000c0002.png",
+        amiiboSeries: "Super Smash Bros.",
+        character: "Luigi",
+        gameSeries: "Super Mario",
+        head: "00010000",
+        release: {
+          au: "2014-11-29",
+          eu: "2014-11-28",
+          jp: "2014-12-06",
+          na: "2014-11-21",
+        },
+        type: "Figure",
+      },
+    ]
+
+    /*   vi.mock("@/services", () => ({
+      getCharacters: vi.fn(() => Promise.resolve(characters)),
+    })) */
+
+    //const { characters } = await render(getStaticProps)
+
+    render(<Home characters={characters2} />)
+
+    const mario = screen.getByText("Mario")
+    const luigi = screen.getByText("Luigi")
+
+    expect(mario).toBeDefined()
+    expect(luigi).toBeDefined()
+
+    screen.debug()
+  })
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,24 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    fs: {
+      strict: true, // Habilita la opción 'strict' para el sistema de archivos del servidor
+    },
+  },
+
+  resolve: {
+    alias: {
+      "@": "/src", // Define un alias para la carpeta 'src' usando el símbolo '@'
+    },
+  },
+  plugins: [react()], // Agrega el plugin de React para trabajar con componentes de React en los tests
   test: {
-    environment: "jsdom",
+    environment: "jsdom", // Configura el entorno de pruebas como 'jsdom' para simular un entorno de navegador
+    globals: true, // Habilita el acceso a las variables globales en el entorno de pruebas
   },
 })
